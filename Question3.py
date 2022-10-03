f527aca8dcc3a2c4454692c91f4bb9c56f533ec4
BairagiTarun <112105779+BairagiTarun@users.noreply.github.com>
2022-10-03 22:59:31 +0530

    Create Question3.py

---
+++ Question3.py
@@ -0,0 +1,6 @@
+var1=int(input())
+var2=bool(input())
+var3=str(input())
+var4=float(input())
+var5=complex(int(input()),int(input()))
+print(type(var1),type(var2),type(var3),type(var4),type(var5),sep='\n')
